2aed3dec8fc494bdd1fa0779337198134c0e2201
alexmorten <alexandrius.martinus@gmail.com>
2019-03-13 17:04:05 +0100

    first commit

---
+++ Blink.ino
@@ -0,0 +1,23 @@
+/*
+ * Blinks two LEDs when motion detected
+ */
+
+//variables
+int led1 = 13;
+int wait = 2000; //500ms = 1/2s
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(led1, 1);
+  delay(wait);
+  digitalWrite(led1, LOW);
+  delay(wait);
+}
---
+++ Makefile
@@ -0,0 +1,3 @@
+deploy:
+	scp Blink.ino pi@10.0.5.63:~/Arduino/Blink/
+	ssh pi@10.0.5.63 "cd Arduino/Blink; amake -u uno Blink.ino /dev/ttyACM0"
