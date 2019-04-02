cd5d264d6be542d374cfea68bf13367b3b9e7770
alexmorten <alexandrius.martinus@gmail.com>
2019-04-02 11:06:19 +0200

    copy mesh network code from pi

--- Receiver.ino
+++ Receiver.ino
@@ -1,25 +1,43 @@
-#include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
+#define RH_MESH_MAX_MESSAGE_LEN 50
 
-RH_ASK driver(2000, 11, 12); // Setting RX pin (receiver) to 2 and TX pin (transmitter) to 3
+#include <RHMesh.h>
+#include <RH_ASK.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 1
+#define SERVER1_ADDRESS 2
+
+RH_ASK driver(2000, A5, 11);
+
+RHMesh manager(driver, SERVER1_ADDRESS);
 
 void setup()
 {
-  Serial.begin(9600); // Debugging only
-  if (!driver.init())
+  Serial.begin(9600);
+  if (!manager.init())
     Serial.println("init failed");
-  driver.setModeRx();
 }
+
+uint8_t data[] = "And hello back to you from node_b";
+uint8_t buf[RH_MESH_MAX_MESSAGE_LEN];
 
 void loop()
 {
-  uint8_t buf[12];
-  uint8_t buflen = sizeof(buf);
-  if (driver.recv(buf, &buflen)) // Non-blocking
+  //uint8_t arp = manager.doArp(SERVER1_ADDRESS);
+  //Serial.println(arp)
+
+  uint8_t len = sizeof(buf);
+  uint8_t from;
+  if (manager.recvfromAck(buf, &len, &from))
   {
-    int i;
-    // Message with a good checksum received, dump it.
-    Serial.print("Message: ");
+    Serial.print("got request from : 0x");
+    Serial.print(from, HEX);
+    Serial.print(": ");
     Serial.println((char *)buf);
+    // Send a reply back to the originator client
+    uint8_t code = manager.sendtoWait(data, sizeof(data), from);
+    Serial.println(code);
+    if (code != RH_ROUTER_ERROR_NONE)
+      Serial.println("sendtoWait failed");
   }
 }
--- Transmitter.ino
+++ Transmitter.ino
@@ -1,20 +1,59 @@
-#include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
+#define RH_MESH_MAX_MESSAGE_LEN 50
 
-RH_ASK driver(2000, 11, 12); // Setting RX pin (receiver) to 2 and TX pin (transmitter) to 3
+#include <RHMesh.h>
+#include <RH_ASK.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 1
+#define SERVER1_ADDRESS 2
+
+RH_ASK driver(2000, A5, 11);
+RHMesh manager(driver, CLIENT_ADDRESS);
 
 void setup()
 {
-  Serial.begin(9600); // Debugging only
-  if (!driver.init())
+  Serial.begin(9600);
+  if (!manager.init())
     Serial.println("init failed");
-  driver.setModeTx();
 }
+
+uint8_t data[] = "Hello Mesh World!";
+// Dont put this on the stack:
+uint8_t buf[RH_MESH_MAX_MESSAGE_LEN];
 
 void loop()
 {
-  const char *msg = "Hello World!";
-  driver.send((uint8_t *)msg, strlen(msg));
-  driver.waitPacketSent();
-  delay(1000);
+  //uint8_t arp = manager.doArp(SERVER1_ADDRESS);
+  //Serial.println(arp)
+
+  Serial.println("Sending to manager_mesh_node_b");
+
+  // Send a message to a ask_mesh_server
+  // A route to the destination will be automatically discovered.
+
+  uint8_t code = manager.sendtoWait(data, sizeof(data), SERVER1_ADDRESS);
+  Serial.println(code);
+  //Serial.println(RH_ROUTER_ERROR_NONE);
+
+  if (code == RH_ROUTER_ERROR_NONE || code == 2)
+  {
+    // It has been reliably delivered to the next node.
+    // Now wait for a reply from the ultimate server
+    Serial.println("route was found");
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAckTimeout(buf, &len, 10000, &from))
+    {
+      Serial.print("got reply from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char *)buf);
+    }
+    else
+    {
+      Serial.println("No reply, is ask_mesh_node_b running?");
+    }
+  }
+  else
+    Serial.println("sendtoWait failed. Are the intermediate mesh servers running?");
 }
